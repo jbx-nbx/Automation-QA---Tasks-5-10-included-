685c3c478e032a5302dab3cad9ee63fe7aedc4e0
NJ <200839132+jbx-nbx@users.noreply.github.com>
2025-11-30 20:09:29 +0200

    Add files via upload

---
+++ Task 13/HW13.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class HW13
+    {
+        public static bool balancedNum(int[] arr)
+        {
+            int positiveCount = 0, negativeCount =0;
+            if (arr == null || arr.Length == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positiveCount++;
+                }
+                else if (arr[i] < 0)
+                {
+                    negativeCount++;
+                }
+            }
+
+            if (positiveCount == negativeCount)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+    }
+}
---
+++ Task 13/HW13Tests.cs
@@ -0,0 +1,72 @@
+﻿using App1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1Tests
+{
+    [TestClass]
+    public class HW13Tests
+    {
+        [TestMethod]
+        public void balanceCheck_null_ReturnTrue()
+        {
+            //Arrange
+            int[] balance = null;
+            //Act
+            bool result = HW13.balancedNum(balance);
+            //Assert
+
+            Assert.IsFalse(result, "Null array should return False !!!!");
+        }
+
+
+        [TestMethod]
+        public void balanceCheck_emptyArr_ReturnTrue()
+        {
+            //Arrange
+            int[] balance = { };
+            //Act
+            bool result = HW13.balancedNum(balance);
+            //Assert
+            Assert.IsFalse(result, "Empty array should return False !!!!");
+        }
+
+        [TestMethod]
+        public void balanceCheck_balancedArr_ReturnTrue()
+        {
+            //Arrange
+            int[] balance = { -1, 2, -3, 4, -5, 6 };
+            //Act
+            bool result = HW13.balancedNum(balance);
+            //Assert
+            Assert.IsTrue(result, "Balanced array should return True !!!!");
+        }
+
+        [TestMethod]
+        public void balanceCheck_notBalancedArr_ReturnFalse()
+        {
+            //Arrange
+            int[] balance = { 1, 2, 3, 4, 5 };
+            //Act
+            bool result = HW13.balancedNum(balance);
+            //Assert
+            Assert.IsFalse(result, "Not Balanced array should return False !!!!");
+        }
+
+        [TestMethod]
+        public void balanceCheck_singleElementArr_ReturnFalse()
+        {
+            //Arrange
+            int[] balance = { 5 };
+            //Act
+            bool result = HW13.balancedNum(balance);
+            //Assert
+            Assert.IsFalse(result, "Single element array should return False !!!!");
+
+        }
+    }
+}
